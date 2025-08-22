56805550979b9394079bde1568848f26b53477d6
Ibrar Ali <Ibrar.ali@invobyte.com>
2025-08-22 21:20:45 +0500

    Premetive data types

--- 02_DataTypes.js
+++ 02_DataTypes.js
@@ -1,1 +1,21 @@
 "Use Strict" //Treat all code of this component as new version of javascript 
+//alert cannot be used in node js code runner , it can be used in the browser 
+
+let name = 'ibrar'
+let age = 18
+let isLoggedIn = false
+
+
+
+//  /*Premetive DataTypes*/
+// number 2 power of 53
+//bigInt  ( used in the trading  , facebook , stcok)
+// string
+//null  it is a standalone value
+//undefined   => is that the value is not defined yet  i.e let state = 
+//symbol  => used for the uniqueness 
+
+
+///Objects 
+console.log(typeof null) ; Object 
+console.log(typeof undefined); undefined
