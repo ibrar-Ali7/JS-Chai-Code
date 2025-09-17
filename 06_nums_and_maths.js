6427f59839f0bbe3f158abd0ac0aae08b90059c9
Ibrar Ali <Ibrar.ali@invobyte.com>
2025-09-17 18:12:08 +0500

    Math and numbers

--- 06_nums_and_maths.js
+++ 06_nums_and_maths.js
@@ -1,4 +1,21 @@
 const scrore = 400;
-console.log(scrore)
+// console.log(scrore)
 const bonus = new Number(400)
-console.log(bonus)
+// console.log(bonus)
+
+const newAmount = 170.897
+// console.log(newAmount.toPrecision(3))
+const newValues  = 1000000.224
+// console.log(newValues.toLocaleString())
+// console.log(Math)
+// console.log(Math.abs(-500))  // Absolute value . It will convert the negetive value to positive value & will not change the postive value to negetive value//
+// // console.log(Math.round (4.66)) // It will round off the value to nearest integer 
+// // console.log(Math.floor(4.99)) // It will round off the value to lower integer 
+// // console.log(Math.ceil(4.01)) // It will round off the value to upper integer 
+// console.log((Math.random()*3)+1) // It will generate the value from 1 to 3 
+// console.log(Math.ceil(Math.random()*4)) // It will generate the random number between 1 to 4 
+// console.log(Math.ceil(Math.random()*4 + 1)) // It will generate the random number between 1 to 4//
+
+const Min = 10 
+const Max = 20 
+console.log(Math.floor(Math.random () *(Max - Min + 1 ))+Min) // It will generate the random number between 10 to 20 //
